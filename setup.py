09321b58125e37529138ab3e45c1ea3379cd5fec
Karataev Pavel <karataev.pavel@gmail.com>
2015-03-26 17:01:42 +0300

    Migrate setup.py to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='hashable',
-    version='1.0',
-    description='Creates a __hash__ method',
+    version='0.1',
+    description='Allow objects to be hashable and comparable by attributes',
     author='Karataev Pavel',
     author_email='karataev.pavel@gmail.com',
     url='https://github.com/minmax/hashable',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
